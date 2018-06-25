2833f5ea60c93c0302e29cd313a51ce984f57f03
HopeMWatson <40265898+HopeMWatson@users.noreply.github.com>
2018-06-25 13:29:16 +0100

    Update intro-hope.R

--- intro-hope.R
+++ intro-hope.R
@@ -88,44 +88,6 @@ typeof(p) #checking p is double, will cast to list
 listp <- list(p) # casting p-values as list 
 pp <- finemap.abf(listp) #running finemap.abf as list 
 ## 3. run the result through finemap.abf() to get posterior probabilities
-                                   library(coloc)
-setClass("simdata",
-         representation(df1="data.frame",df2="data.frame"))
-setValidity("simdata", function(object) {
-  n <- nrow(object@df1)
-  if(nrow(object@df2)!=n)
-    return("nrow of '@df1' should equal nrow of '@df2'")
-})
-setMethod("show", signature="simdata", function(object) {
-  cat("pair of simulated datasets, with",ncol(object@df1)-1,"SNPs and",nrow(object@df1),"samples.\n")
-})
-
-sim.data <- function(nsnps=50,nsamples=200,causals=1:2,nsim=1) {
-  cat("Generate",nsim,"small sets of data\n")
-  ntotal <- nsnps * nsamples * nsim
-  X1 <- matrix(rbinom(ntotal,1,0.4)+rbinom(ntotal,1,0.4),ncol=nsnps)
-  Y1 <- rnorm(nsamples,rowSums(X1[,causals]),2)
-  X2 <- matrix(rbinom(ntotal,1,0.4)+rbinom(ntotal,1,0.4),ncol=nsnps)
-  Y2 <- rnorm(nsamples,rowSums(X2[,causals]),2)
-  colnames(X1) <- colnames(X2) <- paste("s",1:nsnps,sep="")
-  df1 <- cbind(Y=Y1,X1)
-  df2 <- cbind(Y=Y2,X2)
-  if(nsim==1) {
-    return(new("simdata",
-               df1=as.data.frame(df1),
-               df2=as.data.frame(df2)))
-  } else {
-    index <- split(1:(nsamples * nsim), rep(1:nsim, nsamples))
-    objects <- lapply(index, function(i) new("simdata", df1=as.data.frame(df1[i,]),
-                                             df2=as.data.frame(df2[i,])))
-    return(objects)
-  }
-}
-
-## simulate some data and load the coloc library
-set.seed(46411)
-data <- sim.data(nsamples=1000,nsim=1)
-library(coloc)
 
 ## 4. use your code from 1 to find the snps in the credible set for these data.
 ## Is the true (simulated) causal variant in the set?
